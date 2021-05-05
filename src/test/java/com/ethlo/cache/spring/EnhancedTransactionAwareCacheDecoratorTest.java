f7a53eba36a6c9da49486ed8df096d7b8e1e50e3
Morten Haraldsen <m.haraldsen@kezzler.com>
2021-05-05 07:07:59 +0200

    Remove unused import

--- src/test/java/com/ethlo/cache/spring/EnhancedTransactionAwareCacheDecoratorTest.java
+++ src/test/java/com/ethlo/cache/spring/EnhancedTransactionAwareCacheDecoratorTest.java
@@ -31,7 +31,6 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.jcache.JCacheCache;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @RunWith(PowerMockRunner.class)
